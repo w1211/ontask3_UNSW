24badebc17a268a906e6d19c165f192c6a2b7b39
stevenphaedonos <stevenphaedonos@gmail.com>
2017-12-06 23:22:35 +1100

    Update datasource method Update postman examples

--- backend/datasource/models.py
+++ backend/datasource/models.py
@@ -1,20 +1,22 @@
 from mongoengine import Document, EmbeddedDocument, DynamicEmbeddedDocument, fields
+from datetime import datetime
 
 from container.models import Container
+
 
 class Connection(EmbeddedDocument):
     dbType = fields.StringField(choices=('mysql', 'postgresql', 'sqlite', 'mssql'), required=True)
     host = fields.StringField(required=True)
     database = fields.StringField(required=True)
     user = fields.StringField(required=True)
-    password = fields.StringField(required=True)
+    password = fields.StringField(null=True)
     query = fields.StringField(required=True)
 
 class Metadata(EmbeddedDocument):
     name = fields.StringField(required=True)
-    isDynamic = fields.BooleanField(required=True)
-    updateFrequency = fields.StringField(choices=('', 'hourly', 'daily', 'weekly'), required=True) # Weekly = 7 days from lastUpdated
-    lastUpdated = fields.DateTimeField()
+    isDynamic = fields.BooleanField()
+    updateFrequency = fields.StringField(choices=('hourly', 'daily', 'weekly')) # Weekly = 7 days from lastUpdated
+    lastUpdated = fields.DateTimeField(default=datetime.now)
 
 class DataSource(Document):
     owner = fields.IntField()
--- backend/datasource/views.py
+++ backend/datasource/views.py
@@ -13,75 +13,49 @@ from ontask.settings import DATASOURCE_KEY
 from .serializers import DataSourceSerializer
 from .models import DataSource
 from matrix.models import Matrix
-from ontask.permissions import IsOwner
+from ontask.permissions import IsOwnerOrShared
 
 
 class DataSourceViewSet(viewsets.ModelViewSet):
     lookup_field = 'id'
     serializer_class = DataSourceSerializer
-    permission_classes = [IsOwner]
+    permission_classes = [IsOwnerOrShared]
 
     def get_queryset(self):
         return DataSource.objects.all()
 
-    def perform_create(self, serializer):
-        # Check that the data source to be created is unique on (owner, name)
-        queryset = DataSource.objects.filter(
-            owner = self.request.user.id,
-            metadata__name = self.request.data['name']
-        )
-        if queryset.count():
-            raise ValidationError('A data source with this name already exists')
-
-        # Encrypt the db password of the data source
-        cipher = Fernet(DATASOURCE_KEY)
-        password_cipher = cipher.encrypt(str(self.request.data['password']))
-
-        # Map attributes from the http request to the subdocument schemas
-        connection = {}
-        connection['dbType'] = self.request.data['dbType']
-        connection['host'] = self.request.data['host']
-        connection['database'] = self.request.data['database']
-        connection['user'] = self.request.data['user']
-        connection['password'] = password_cipher
-        connection['query'] = self.request.data['query']
-
-        metadata = {}
-        metadata['name'] = self.request.data['name']
-        metadata['isDynamic'] = (self.request.data['isDynamic'] == 'true') # Convert string to boolean
-        metadata['updateFrequency'] = self.request.data['updateFrequency']
-        metadata['lastUpdated'] = datetime.now()
-
-        container = self.request.data['container']
-
-        # Connect to specified database and get the data from the query
-        # Data passed in to the DataSource model must be a list of dicts of the form {column_name: value}
-        # TO DO: if isDynamic, then store values into lists as objects with timestamps
+    def get_datasource_data(self, connection):
         if connection['dbType'] == 'mysql':
-            dbConnection = mysql.connector.connect(
-                host = connection['host'],
-                database = connection['database'],
-                user = connection['user'],
-                password = self.request.data['password'] # Use the unencrypted password
-            )
-            cursor = dbConnection.cursor(dictionary=True)
-            cursor.execute(connection['query'])
-            data = list(cursor)
-            cursor.close()
-            dbConnection.close()
-
+            try:
+                dbConnection = mysql.connector.connect(
+                    host = connection['host'],
+                    database = connection['database'],
+                    user = connection['user'],
+                    password = connection['password']
+                )
+                cursor = dbConnection.cursor(dictionary=True)
+                cursor.execute(connection['query'])
+                data = list(cursor)
+                cursor.close()
+                dbConnection.close()
+            except: 
+                raise ValidationError('Error connecting to database')
+                
         elif connection['dbType'] == 'postgresql':
-            dbConnection = psycopg2.connect(
-                host = connection['host'],
-                dbname = connection['database'],
-                user = connection['user'],
-                password = self.request.data['password'] # Use the unencrypted password
-            )
-            cursor = dbConnection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-            cursor.execute(connection['query'])
-            data = list(cursor)
-            cursor.close()
-            dbConnection.close()
+            try:
+                dbConnection = psycopg2.connect(
+                    host = connection['host'],
+                    dbname = connection['database'],
+                    user = connection['user'],
+                    password = connection['password']
+                )
+                cursor = dbConnection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+                cursor.execute(connection['query'])
+                data = list(cursor)
+                cursor.close()
+                dbConnection.close()
+            except: 
+                raise ValidationError('Error connecting to database')
 
         # TO DO: implement MS SQL and SQLite imports
         elif connection['dbType'] == 'sqlite':
@@ -90,14 +64,73 @@ class DataSourceViewSet(viewsets.ModelViewSet):
         elif connection['dbType'] == 'mssql':
             pass
 
+        if not len(data):
+            raise ValidationError('The query returned no data')
+
+        return data
+
+    def perform_create(self, serializer):
+        # Check that the data source to be created is unique on (owner, name)
+        queryset = DataSource.objects.filter(
+            owner = self.request.user.id,
+            metadata__name = self.request.data['metadata']['name']
+        )
+        if queryset.count():
+            raise ValidationError('A data source with this name already exists')
+
+        # Connect to specified database and get the data from the query
+        # Data passed in to the DataSource model must be a list of dicts of the form {column_name: value}
+        # TO DO: if isDynamic, then store values into lists as objects with timestamps
+        connection = self.request.data['connection']
+        data = self.get_datasource_data(connection)
+
+        # Encrypt the db password of the data source
+        cipher = Fernet(DATASOURCE_KEY)
+        connection['password'] = cipher.encrypt(str(connection['password']))
+
         serializer.save(
             owner = self.request.user.id,
-            container = container,
             connection = connection,
-            metadata = metadata,
             data = data
         )
 
+    def perform_update(self, serializer):
+        self.check_object_permissions(self.request, self.get_object())
+        # Check that the data source to be created is unique on (owner, name)
+        queryset = DataSource.objects.filter(
+            # We only want to check against the documents that are not the document being updated
+            # I.e. only include objects in the filter that do not have the same id as the current object
+            # id != self.kwargs.get(self.lookup_field) is syntactically incorrect
+            # So instead we are making use of a query operator [field]__ne (i.e. field not equal to)
+            # Refer to http://docs.mongoengine.org/guide/querying.html#query-operators for more information
+            id__ne = self.kwargs.get(self.lookup_field), # Get the id of the object from the url route as defined by the lookup_field
+            owner = self.request.user.id,
+            metadata__name = self.request.data['metadata']['name']
+        )
+        if queryset.count():
+            raise ValidationError('A data source with this name already exists')
+
+        # Connect to specified database and get the data from the query
+        # Data passed in to the DataSource model must be a list of dicts of the form {column_name: value}
+        # TO DO: if isDynamic, then store values into lists as objects with timestamps
+        connection = self.request.data['connection']
+        data = self.get_datasource_data(connection)
+        
+        # Encrypt the db password of the data source
+        cipher = Fernet(DATASOURCE_KEY)
+        if connection['password']:
+            # If a new password is provided then encrypt it and overwrite the old one
+            connection['password'] = cipher.encrypt(str(connection['password']))
+        else:
+            # Otherwise simply keep the old password (which is already encrypted)
+            connection['password'] = self.get_object()['connection']['password']
+
+        serializer.save(
+            owner = self.request.user.id,
+            connection = connection,
+            data = data
+        )
+        
     def perform_destroy(self, obj):
          # Ensure that the request.user is the owner of the object
         self.check_object_permissions(self.request, obj)
