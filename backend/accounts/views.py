a4e1e3f7bc9d0362ea774a1ba6c1a568ed280805
Rohit Jose <rohitjose@gmail.com>
2017-12-07 14:07:46 +1100

    Adds the APIs for the login methods

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,6 +1,62 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from rest_framework.views import APIView
+from django.contrib.auth import login
+from rest_framework.response import Response
+from rest_framework.status import HTTP_403_FORBIDDEN, HTTP_401_UNAUTHORIZED
+from rest_framework.authtoken.models import Token
+from .auth_wrapper import AAFAuthHandler, LTIAuthHandler
 
 from django.shortcuts import render
 
-# Create your views here.
+class AAFAuthRouter(APIView):
+    '''Hosts the logic to handle the post request from AAF and authenticate the
+    user to the application'''
+
+    authentication_classes = ()
+    permission_classes = ()
+    def post(self, request, format=None):
+        '''Handles the post request from AAF Rapid Connect and routes the user
+        to the landing page of the application'''
+
+        aaf_authhandler = AAFAuthHandler()
+        user = aaf_authhandler.authenticate_user(request.data)
+
+        response = {}
+        # Check for invalid user login
+        if user == "NOT_AUTHORIZED":
+            response =  Response({"error": "Not Authorized"}, status=HTTP_403_FORBIDDEN)
+        elif user == "EXPIRED_SIGNATURE":
+            response = Response({"error": "Expired Signature"}, status=HTTP_403_FORBIDDEN)
+        elif not user:
+            response = Response({"error": "Login failed"}, status=HTTP_401_UNAUTHORIZED)
+
+
+        # Check for a valid user login
+        if(user):
+            login(request, user)
+            token, _ = Token.objects.get_or_create(user=user)
+            response =  Response({"token": token.key, "user": user})
+        
+        return response
+        
+        
+class LTIAuthRouter(APIView):
+    '''Hosts the logic to handle the post request from LTI and authenticate the
+    user to the application'''
+
+    authentication_classes = ()
+    permission_classes = ()
+    def post(self, request, format=None):
+        '''Handles the post request from LTI and routes the user
+        to the landing page of the application'''
+
+        lti_authhandler = LTIAuthHandler()
+        user = lti_authhandler.authenticate_user(request.data)
+
+        # Check for a valid login
+        if(user):
+            login(request, user)
+
+        return Response("")
+        
