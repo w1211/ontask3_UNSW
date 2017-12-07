e701325396c0cf3588384fd231817b17d9fc8396
Rohit Jose <rohitjose@gmail.com>
2017-12-07 14:34:42 +1100

    Addds the url configuration

---
+++ backend/accounts/urls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^user/auth', views.AAFAuthRouter.as_view()),
+    url(r'^user/lti', views.LTIAuthRouter.as_view())
+]
