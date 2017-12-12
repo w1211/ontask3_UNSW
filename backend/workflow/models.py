06a2158dad18bde313c08beafd53fdb5174b849c
stevenphaedonos <stevenphaedonos@gmail.com>
2017-12-12 15:08:58 +1100

    Condition groups for enforcing mutual exclusivity

--- backend/workflow/models.py
+++ backend/workflow/models.py
@@ -7,9 +7,13 @@ class Condition(EmbeddedDocument):
     name = fields.StringField(required=True)
     formula = fields.StringField(required=True)
 
+class ConditionGroup(EmbeddedDocument):
+    name = fields.StringField(required=True)
+    conditions = fields.EmbeddedDocumentListField(Condition)
+
 class Workflow(Document):
     owner = fields.IntField()
     name = fields.StringField(required=True)#, unique_with='matrix')
     matrix = fields.ReferenceField(Matrix, required=True, reverse_delete_rule=2) # Cascade delete if matrix is deleted
     filter = fields.StringField(null=True)
-    conditions = fields.EmbeddedDocumentListField(Condition)
+    conditionGroups = fields.EmbeddedDocumentListField(ConditionGroup)
--- backend/workflow/views.py
+++ backend/workflow/views.py
@@ -36,7 +36,7 @@ class WorkflowViewSet(viewsets.ModelViewSet):
         # The resulting value is then used to replace "{{field_name}}" (group 1) in the formula
         return re.sub(r'({{([^\s]+)}})', lambda match: self.substitute_value(match.group(2), secondary_columns, item), formula)
 
-    def evaluate_conditions(self, matrix, filter, conditions):
+    def evaluate_conditions(self, matrix, filter, condition_groups):
         data = combine_data(matrix) # combine_data method from Matrix view
         primaryField = matrix['primaryColumn'].field
 
@@ -57,32 +57,40 @@ class WorkflowViewSet(viewsets.ModelViewSet):
         conditions_passed = defaultdict(list)
         # Iterate over the rows in the data and return any rows which pass true
         for item in filtered_data:
-            for condition in conditions:
-                populated_formula = self.populate_formula(matrix['secondaryColumns'], item, condition['formula'])
-                # Eval the populated formula to see if the condition passes for this row
-                # If the row passes the condition, then add the row to the dict of conditions
-                try:
-                    # TO DO: consider security implications of using eval()
-                    if eval(populated_formula):
-                        conditions_passed[condition['name']].append(item)
-                except:
-                    raise ValidationError('An issue occured while trying to evaluate the condition. Do each of the fields used in the formula have the correct \'type\' set?')        
+            for condition_group in condition_groups:
+                # Ensure that each item passes the test for only one condition per condition group
+                hasMatched = False
+                for condition in condition_group['conditions']:
+                    populated_formula = self.populate_formula(matrix['secondaryColumns'], item, condition['formula'])
+                    # Eval the populated formula to see if the condition passes for this row
+                    # If the row passes the condition, then add the row to the dict of conditions
+                    try:
+                        # TO DO: consider security implications of using eval()
+                        if eval(populated_formula):
+                            if hasMatched:
+                                raise ValidationError('An item has matched with more than one condition in the group \'{0}\''.format(condition_group['name']))        
+                            else:
+                                hasMatched = True
+                                conditions_passed[condition['name']].append(item)
+                    except NameError:
+                        raise ValidationError('An issue occured while trying to evaluate the condition. Do each of the fields used in the formula have the correct \'type\' set?')        
         return conditions_passed
 
     def perform_create(self, serializer):
         matrix = Matrix.objects.get(id=self.request.data['matrix'])
         filter = self.request.data['filter']
-        conditions = self.request.data['conditions']
+        condition_groups = self.request.data['conditionGroups']
 
         # Confirm that all provided fields are defined in the matrix
         secondary_columns = [secondary_column.field for secondary_column in matrix['secondaryColumns']]
-        for condition in conditions:
-            formula = condition['formula']
-            fields = re.findall(r'{{([^\s]+)}}', formula)
-            for field in fields:
-                if field not in secondary_columns:
-                    raise ValidationError('Invalid formula: field \'{0}\' does not exist in the matrix'.format(field))
+        for condition_group in condition_groups:
+            for condition in condition_group['conditions']:
+                formula = condition['formula']
+                fields = re.findall(r'{{([^\s]+)}}', formula)
+                for field in fields:
+                    if field not in secondary_columns:
+                        raise ValidationError('Invalid formula: field \'{0}\' does not exist in the matrix'.format(field))
 
-        conditions_passed = self.evaluate_conditions(matrix, filter, conditions)
+        conditions_passed = self.evaluate_conditions(matrix, filter, condition_groups)
         
         serializer.save(owner=self.request.user.id)
