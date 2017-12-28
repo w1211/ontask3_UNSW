84d3d931acf5108543c732c9f6163815e5deae36
stevenphaedonos <stevenphaedonos@gmail.com>
2017-12-28 20:34:51 +1100

    Preliminary workflow child routing

--- frontend/src/workflow/Workflow.js
+++ frontend/src/workflow/Workflow.js
@@ -3,13 +3,34 @@ import React from 'react';
 // import { connect } from 'react-redux';
 // import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Icon, Button, Modal, notification } from 'antd';
+import { Layout, Menu, Icon, Button, Modal, notification } from 'antd';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+
+const { SubMenu } = Menu;
+const { Header, Content, Footer, Sider } = Layout;
 
 // import { fetchContainers } from './ContainerActions';
 
 // import * as ContainerActionCreators from './ContainerActions';
 
 // const confirm = Modal.confirm;
+
+
+const MatrixDefinition = () => (
+  <div>
+    matrix definition view
+  </div>
+);
+const DataView = () => (
+  <div>
+    data view
+  </div>
+);
+const Rules = () => (
+  <div>
+    rules view
+  </div>
+);
 
 
 class Workflow extends React.Component {
@@ -25,15 +46,57 @@ class Workflow extends React.Component {
   // }
 
   render() {
-    // const { 
-    //   containers, dispatch
-    // } = this.props;
-
+    const { 
+      dispatch, match, location
+    } = this.props;
+    
     return (
-      <div style={{display: 'flex', alignItems: 'center', marginBottom: '1em'}}>
-        <Link to="/containers"><Button shape="circle" icon="arrow-left" style={{marginRight: '10px'}}/></Link>
-        <h1 style={{display: 'inline-block', margin: 0}}>Workflow wow!</h1>
-      </div>
+      <Layout style={{ background: '#fff' }}>
+        <Sider width={200}>
+          <Menu
+            mode="inline"
+            selectedKeys={[location.pathname.split("/")[3]]}
+            style={{ height: '100%' }}
+          >
+            <Menu.Item key="back">
+              <Link to="/containers">
+                <Icon type="arrow-left" />
+                <span>Back to containers</span>
+              </Link>
+            </Menu.Item>
+            <Menu.Divider/>
+            <Menu.Item key="matrix">
+              <Link to={`${match.url}/matrix`}>
+                <Icon type="appstore" />
+                <span>Matrix definition</span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="data">
+              <Link to={`${match.url}/data`}>
+                <Icon type="table" />
+                <span>Data view</span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="rules">
+              <Link to={`${match.url}/rules`}>
+                <Icon type="form" />
+                <span>Rules</span>
+              </Link>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Content style={{ padding: '0 24px', minHeight: 280 }}>
+          <div style={{display: 'flex', alignItems: 'center', marginBottom: '1em'}}>
+            <h1 style={{display: 'inline-block', margin: 0}}>Workflow</h1>
+          </div>
+          <Switch>
+            <Redirect exact from={match.url} to={`${match.url}/matrix`}/>
+            <Route exact path={`${match.path}/matrix`} component={MatrixDefinition}/>
+            <Route exact path={`${match.path}/data`} component={DataView}/>
+            <Route exact path={`${match.path}/rules`} component={Rules}/>
+          </Switch>
+        </Content>
+      </Layout>
     );
   };
 
