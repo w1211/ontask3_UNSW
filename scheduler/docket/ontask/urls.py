3e5745b25fed4d5aaf8781ed79b548e7c62d3f4e
Rohit Jose <rohitjose@gmail.com>
2018-02-05 11:55:52 +1100

    Updates the URL endpoint for the ontask API

--- scheduler/docket/ontask/urls.py
+++ scheduler/docket/ontask/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'ontask'
+
 urlpatterns = [
-    url(r'^create', views.CreateDataSourceUpdateTaskView.as_view())
+    url(r'^datasource', views.CreateDataSourceUpdateTaskView.as_view())
 ]
