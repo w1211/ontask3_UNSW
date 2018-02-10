5bcf4792426a1e438fb974347c350ddb346a33ed
Rohit Jose <rohitjose@gmail.com>
2018-02-11 08:50:07 +1100

    Adds the API for scheduling the task

--- scheduler/docket/ontask/views.py
+++ scheduler/docket/ontask/views.py
@@ -16,7 +16,7 @@ from .tasks import update_data_in_data_container
 
 
 class DataSourceUpdateTaskView(APIView):
-    ''' Container for the Ontask Task Scheduler methods'''
+    ''' Container for the Data source update'''
     # Assigns the authentication permissions
     authentication_classes = ()
     permission_classes = ()
@@ -55,4 +55,47 @@ class DataSourceUpdateTaskView(APIView):
         task.delete()
 
         return Response({'task_name':task.name,'message':'Periodic task deleted'})
+
+class WorkflowTaskView(APIView):
+    ''' Container for the Rules execution task scheduler '''
+    # Assigns the authentication permissions
+    authentication_classes = ()
+    permission_classes = ()
+    
+    def post(self, request, format=None):
+        '''Post handle to create a schedule'''
+
+        task_name = "workflow_notification_task_" + str(uuid4())
+        arguments= '{"workflow_id":"' + request.data['workflow_id'] + '"}'
+        
+        schedule, _ = CrontabSchedule.objects.get_or_create(
+            minute = request.data['schedule']['minute'],
+            hour = request.data['schedule']['hour'],
+            day_of_week = request.data['schedule']['day_of_week'],
+            day_of_month = request.data['schedule']['day_of_month'],
+            month_of_year = request.data['schedule']['month_of_year'],
+        )
+
+        periodic_task = PeriodicTask.objects.create(
+           crontab=schedule,
+           name=task_name,
+           task='ontask.tasks.execute_rules',
+           kwargs=arguments
+        )
+
+        print("################ {Periodic Task} ###################")
+        print(periodic_task)
+
+        return Response({'task_name': task_name})
+    
+    def delete(self, request, format=None):
+        '''DELETE handle to remove a scheduler from the beat schedule'''
+        print(self.request.data)
+        task = PeriodicTask.objects.get(name=request.data['name'])
+        print(task)
+        task.delete()
+
+        return Response({'task_name':task.name,'message':'Periodic task deleted'})
+
+
 
