9c8230da40f428a43105b2700edb2585b97c81a5
liu <liuxiaojing1026@gmail.com>
2018-02-14 15:28:42 +1100

    Updated login page (img will be changed)

--- frontend/src/login/Login.js
+++ frontend/src/login/Login.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import { Form, Icon, Input, Button, Alert } from 'antd';
+import { Row, Col, Tabs, Form, Icon, Input, Button, Alert } from 'antd';
 import requestWrapper from '../shared/requestWrapper';
 
 import aaf from '../img/aaf.png';
+import loginImg from '../img/loginImg.png';
 
 const queryString = require('query-string');
 
 const FormItem = Form.Item;
+const TabPane = Tabs.TabPane;
 
+const LoginImgStyle = {
+  width: '500px',
+  margin: '16px 28px 16px 0',
+  float: 'left'
+};
 
 class Login extends React.Component {
   state = { error: null };
@@ -24,7 +31,7 @@ class Login extends React.Component {
       const parameters = {
         url: `/user/local/`,
         method: 'POST',
-        errorFn: (error) => { 
+        errorFn: (error) => {
           this.setState({ error: error });
         },
         successFn: (response) => {
@@ -50,7 +57,7 @@ class Login extends React.Component {
       const parameters = {
         url: `/user/token/`,
         method: 'POST',
-        errorFn: (error) => { 
+        errorFn: (error) => {
           this.setState({ error: error });
         },
         successFn: (response) => {
@@ -62,7 +69,7 @@ class Login extends React.Component {
         isUnauthenticated: true
       }
       requestWrapper(parameters);
-      
+
     } else if (authToken) {
       history.push("containers");
     }
@@ -77,31 +84,40 @@ class Login extends React.Component {
         { localStorage.getItem('token') ?
           <Redirect to="/containers"/>
         :
-          <Form>
-            <div style={{ margin: '30px 0', minWidth: 350, border: '1px solid #d9d9d9', padding: 20 }}>
-              <FormItem>
-                {form.getFieldDecorator('email', {
-                  rules: [{ required: true, message: 'Username is required' }],
-                })(
-                  <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-                )}
-              </FormItem>
-              <FormItem>
-                {form.getFieldDecorator('password', {
-                  rules: [{ required: true, message: 'Password is required' }],
-                })(
-                  <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-                )}
-              </FormItem>
-            </div>
-            { this.state.error && <Alert style={{ marginBottom: 20 }} message={this.state.error} type="error"/>}
-            <div style={{ display: 'flex', flexDirection: 'column' }}>
-              <Button style={{ marginBottom: 20 }}size="large" type="primary" onClick={() => { this.handleSubmit() }}>
-                Log in
-              </Button>
-              <a href="https://rapid.aaf.edu.au/jwt/authnrequest/research/QGSS9UUcaI6UXa7v6AL3Yg"><img src={aaf} alt="AAF Logo" style={{ width: 200 }}/></a>
-            </div>
-          </Form>
+          <Row type="flex" justify="center" align="top" gutter={50} style={{padding: '50px'}}>
+            <Col><img src={loginImg} alt="Login" style={LoginImgStyle}/></Col>
+            <Col style={{height:'350px', margin: '30px'}}>
+              <Tabs defaultActiveKey="1" style={{width: '300px', margin: 'auto'}}>
+                  <TabPane tab="Internal Login" key="1">
+                    <Form style={{width: '300px'}}>
+                      <FormItem>
+                          {form.getFieldDecorator('email', {
+                            rules: [{ required: true, message: 'Username is required' }],
+                          })(
+                            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                          )}
+                        </FormItem>
+                        <FormItem>
+                          {form.getFieldDecorator('password', {
+                            rules: [{ required: true, message: 'Password is required' }],
+                          })(
+                            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                          )}
+                        </FormItem>
+                      { this.state.error && <Alert style={{ marginBottom: 20 }} message={this.state.error} type="error"/>}
+                      <div style={{ display: 'flex', flexDirection: 'column' }}>
+                        <Button style={{ marginBottom: 20 }}size="large" type="primary" onClick={() => { this.handleSubmit() }}>
+                          Log in
+                        </Button>
+                      </div>
+                  </Form>
+                  </TabPane>
+                  <TabPane tab="AAF Login" key="2" >
+                    <a href="https://rapid.aaf.edu.au/jwt/authnrequest/research/QGSS9UUcaI6UXa7v6AL3Yg"><img src={aaf} alt="AAF Logo" style={{ width: 200 }}/></a>
+                  </TabPane>
+              </Tabs>
+            </Col>
+          </Row>
         }
       </div>
     );
