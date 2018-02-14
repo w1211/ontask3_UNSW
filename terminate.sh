607b36fab40f9a77c948c22344f0deae9426d075
Steven Phaedonos <stevenphaedonos@gmail.com>
2018-02-15 01:50:29 +1100

    Update terminate shell script to use virtualenv for uWSGI

--- terminate.sh
+++ terminate.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+source virtualenvs/backend/bin/activate
 uwsgi --stop tmp/ontask.pid
 uwsgi --stop tmp/scheduler.pid
+deactivate
 
 echo "---------------------------------"
 echo "- Server termination completed. -"
