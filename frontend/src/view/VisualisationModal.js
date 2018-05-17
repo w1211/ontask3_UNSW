47f09910bd90ecf8403b1f2a1b047b816907f293
Ella Liu <liuxiaojing1026@gmail.com>
2018-05-17 16:59:17 +1000

    Update binning function.

--- frontend/src/view/VisualisationModal.js
+++ frontend/src/view/VisualisationModal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Modal, Select, Slider, InputNumber } from 'antd';
-import { Chart, Geom, Axis, Legend, Coord, Tooltip, Label } from 'bizcharts';
+import { Chart, Geom, Axis, Legend, Coord, Tooltip, Label, Guide } from 'bizcharts';
 import { View as dataView} from '@antv/data-set';
 
 import * as ViewActionCreators from './ViewActions';
@@ -12,6 +12,7 @@ import { track } from "bizcharts";
 track(false);
 
 const {Option} = Select;
+const {Line, Text} = Guide;
 
 class VisualisationModal extends React.Component {
   constructor(props) {
@@ -40,8 +41,36 @@ class VisualisationModal extends React.Component {
     this.boundActionCreators.updateVisualisationChart(selectedId, chartType, numCols, rangeMin, rangeMax);
   };
 
+  onRangeMaxChange = (value, defaultMin, defaultMax) => {
+    const {rangeMin, rangeMax} = this.state;
+    if(rangeMin){
+      if(value>=rangeMin && value<=defaultMax){
+        this.setState({rangeMax: value});
+      }
+    }
+    else{
+      if(value>=defaultMin && value<=defaultMax){
+        this.setState({rangeMax: value});
+      }
+    }
+  }
+
+  onRangeMinChange = (value, defaultMin, defaultMax) => {
+    const {rangeMin, rangeMax} = this.state;
+    if(rangeMax){
+      if(value<=rangeMax && value>=defaultMin){
+        this.setState({rangeMin: value});
+      }
+    }
+    else{
+      if(value<=defaultMax && value>=defaultMin){
+        this.setState({rangeMin: value});
+      }
+    }
+  }
+
   render() {
-    const { visualisation_visible, build, data, visualise, isRowWise } = this.props;
+    const { visualisation_visible, build, data, visualise, isRowWise, record } = this.props;
     const { chartType, numCols, rangeMin, rangeMax } = this.state;
     const MAX_COL = 15;
 
@@ -49,10 +78,10 @@ class VisualisationModal extends React.Component {
     let cols;
     let defaultMax=1;
     let defaultMin=100;
-    let isOverMaxCols=false;
 
     let type;
     let colNameSelected;
+
 
     if (build && data && (this.state.colNameSelected || visualise)) {
 
@@ -60,6 +89,8 @@ class VisualisationModal extends React.Component {
       let fieldName = this.state.colNameSelected ? this.state.colNameSelected.field : visualise.field;
       
       const buildStep = build.steps[buildStepIndex];
+
+
       if (buildStep.type === 'datasource') {
         type = buildStep.datasource.types[fieldName];
         colNameSelected = buildStep.datasource.labels[fieldName];
@@ -67,96 +98,31 @@ class VisualisationModal extends React.Component {
 
       //Barchar ploting
       if (chartType === "barChart" || chartType === "pieChart"){
-
         dv = new dataView()
         .source(data)
-        //pick selected column and drop others
         .transform({type: 'pick', fields: [colNameSelected]})
-        //count based on colNameSelected
-        .transform({type: 'aggregate', fields: [colNameSelected], 
-          operations: 'count', as: 'count',
-          groupBy: [colNameSelected]});
-
-        //if current column type is number, then we can combine bars with user defined intervals
+        .transform({
+          type: 'filter',
+          callback(row) {
+            return row[colNameSelected] !=='' ;
+          }
+        });
         if (type === "number") {
-          //sort current column values
-          dv.transform({type: 'sort', callback(a, b) { 
-            if(a[colNameSelected]!=="" && b[colNameSelected]!==""){
-              return a[colNameSelected] - b[colNameSelected];
-            }
-            //put empty rows at first
-            if(a[colNameSelected]===""){ return -1; }
-            if(b[colNameSelected]===""){ return 1; }
-          }});
-
-          if(dv.rows.length>MAX_COL){
-            isOverMaxCols = true;
-
-            //min and max value for slider
-            defaultMin = Number(dv.rows[0][colNameSelected])==='' ? Number(dv.rows[1][colNameSelected]) : Number(dv.rows[0][colNameSelected]);
-            defaultMax = Number(dv.rows[dv.rows.length-1][colNameSelected]);
-
-            //min and max value for calculating interval
-            const min = rangeMin? rangeMin : defaultMin;
-            const max = rangeMax? rangeMax : defaultMax;
-
-            //last col range will be equal or less than this interval
-            const interval = (max-min)/numCols;
-
-            //if user customize range, we filter out rows with values outside of this range
-            if (rangeMin && rangeMax){
-              dv.transform({
-                type: 'filter',
-                callback(row) {
-                  return row[colNameSelected] >= min && row[colNameSelected] <= max ;
-                }
-              });
-            }
-
-            //mark each column with corresponding interval number, stored in mergeIndex field
-            dv.transform({
-              type: 'map',
-              callback: (row, i)=>{
-                if(row[colNameSelected]===""){ row.mergeIndex = -1; }
-                else{ row.mergeIndex = Math.floor((row[colNameSelected]-min)/interval); }
-                return row;
-            }})
-            //count how many rows are in each interval
-            .transform({
-              type: 'aggregate', fields: ["count"], 
-              operations: 'sum', as: 'summation',
-              groupBy: ["mergeIndex"]
-            })
-            
-            //label each bar with the start and end number of corresponding interval
-            .transform({
-              type: 'map',
-              callback: (row, i)=>{
-                if(row[colNameSelected]===""){
-                  row.axisLabel = "";
-                }
-                else{
-                  let axisMax = (min + interval*(row.mergeIndex+1));
-                  if (axisMax>max){
-                    axisMax = max;
-                  }
-                  row.axisLabel = 
-                    ''+(this.isInt(min + interval*row.mergeIndex)?(min + interval*row.mergeIndex).toString():Number(min + interval*row.mergeIndex).toFixed(2).toString())+'-'
-                    +(this.isInt(axisMax)?axisMax.toString():Number(axisMax).toFixed(2).toString());
-                }
-                return row;
-            }});
-          }      
+          dv.transform({
+            type: 'bin.histogram',
+            field: colNameSelected,
+            binWidth: 5,
+            as: [colNameSelected, 'count']
+          });
         }
         else{
-          //put empty rows at firsts
-          dv.transform({type: 'sort', callback(a, b) { 
-            if(a[colNameSelected]===""){ return -1; }
-            if(b[colNameSelected]===""){ return 1; }
-          }});
+          dv.transform({
+            type: 'aggregate', fields: [colNameSelected], 
+            operations: 'count', as: 'count',
+            groupBy: [colNameSelected]
+          });
         }
 
-        //define columns in bar chart
         if(chartType === "barChart"){
           cols = {
             colNameSelected: {tickInterval: 5},
@@ -164,51 +130,47 @@ class VisualisationModal extends React.Component {
         }
 
         if(chartType === "pieChart"){
-          //define columns in pie chart
           cols = {
             percent: {
               formatter: val => {
                 val = parseFloat(val * 100).toFixed(2) + '%';
                 return val;
           }}};
-          //transform data to percentage form 
-          if(dv.rows[0] && dv.rows[0]["axisLabel"]){
-            dv.transform({type: 'percent', field: 'summation',
-                          dimension: "axisLabel", as: 'percent'});
-          }
-          else{
-            dv.transform({type: 'percent', field: 'count',
-                          dimension: colNameSelected, as: 'percent'});
-          }
+          dv.transform({
+            type: 'percent', field: 'count',
+            dimension: colNameSelected, as: 'percent'
+          })
+          if(type === "number")
+          dv.transform({
+            type: 'map', 
+            callback: (row) => {
+              row[colNameSelected] = row[colNameSelected][0]+'-'+row[colNameSelected][1];
+              return row;
+          }});
         }
       }
 
-      //ploting the box chart
       if(chartType==="boxPlot"){
         dv = new dataView()
         .source(data)
         .transform({type: 'pick', fields: [colNameSelected]})
-        //filter out rows with empty value in colNameSelected
         .transform({
           type: 'filter',
           callback(row) {
               return row[colNameSelected]!=="";
         }})
-        //convert string to number
         .transform({
           type: 'map',
           callback: (obj) => {
             obj[colNameSelected] = Number(obj[colNameSelected]);
             return obj;
         }})
-        //calculate quantile values
         .transform({
           type: 'bin.quantile',
           field: colNameSelected,
           as: 'range',
           fraction: 4
         })
-        //restore q1 median q3 high and low for label later 
         .transform({
           type: 'map',
           callback: (row) => {
@@ -218,7 +180,6 @@ class VisualisationModal extends React.Component {
             return row;
         }});
 
-        //define columns for boxplot
         cols ={
           range: {
             max: dv.rows[0].high
@@ -294,10 +255,38 @@ class VisualisationModal extends React.Component {
       { chartType === "barChart" && dv &&
         <div>
           <Chart height={450} data={dv} scale={cols} forceFit>
-            <Axis name={dv.rows[0] && ("axisLabel" in dv.rows[0])?"axisLabel":colNameSelected} label={{autoRotate: true}} title={colNameSelected} />
+            <Axis 
+              name={colNameSelected} 
+              title={colNameSelected} 
+              autoRotate={true}
+            />
             <Axis title={"Count"} name= {"count"} />
             <Tooltip crosshairs={{type : "y"}} />
-            <Geom type="interval" position={dv.rows[0] && ("axisLabel" in dv.rows[0])? "axisLabel*summation": colNameSelected+"*count"} />
+            <Geom type="interval" position={dv.rows[0] && colNameSelected+"*count"} />
+            {record &&
+              <Guide>
+                <Line
+                  top={true}
+                  start={[record[colNameSelected], 0]}
+                  end={[record[colNameSelected], 'max']}
+                  lineStyle= {{
+                    lineDash:[2,0],
+                    lineWidth: 1
+                  }}
+                />
+                <Text
+                  top= {true}
+                  position= {[record[colNameSelected],'max']} 
+                  content= {"You are here"}
+                  style= {{
+                    fill: '#666',
+                    fontSize: '12'
+                  }}
+                  offsetX= {5}
+                  offsetY= {0.5}
+                />
+              </Guide>
+            }
           </Chart>
         </div>
       }
@@ -318,16 +307,8 @@ class VisualisationModal extends React.Component {
             <Geom
               type="intervalStack"
               position="percent"
-              color={dv.rows[0] && dv.rows[0]["axisLabel"] ? "axisLabel" : colNameSelected}
-              tooltip={dv.rows[0] && dv.rows[0]["axisLabel"] ?
-                ['axisLabel*percent',(percent) => {
-                  percent = parseInt(percent * 100, 10) + '%';
-                  return {
-                    name: dv.rows[0]["axisLabel"],
-                    value: percent
-                  };
-                }]
-                :
+              color={colNameSelected}
+              tooltip={dv.rows[0] && colNameSelected &&
                 [colNameSelected+'*percent',(colNameSelected, percent) => {
                   percent = parseInt(percent * 100, 10) + '%';
                   return {
@@ -336,62 +317,60 @@ class VisualisationModal extends React.Component {
                   };
                 }]
               }
-              style={{lineWidth: 1,stroke: '#fff'}}
+              style={{lineWidth: 1, stroke: '#fff'}}
               >
-              <Label 
-                content='percent' 
+              <Label
+                content='percent'
                 formatter={(val, item) => {
-                  if (dv.rows[0] && dv.rows[0]["axisLabel"]){ 
-                    return item.point["axisLabel"] + ': ' + val;
-                  }
-                  else{
+                  if (dv.rows[0]){ 
                     return item.point[colNameSelected] + ': ' + val;
                   }
-                }} 
+                }}
               />
             </Geom>
           </Chart>
         </div>
       }
 
-      { isOverMaxCols &&
-      <div>
-        <div style={{display: "flex", justifyContent: "left", marginBottom: 10, alignItems: "center"}}>
-          <h4>Interval: </h4>
-          <InputNumber min={1}
-            style={{display: "flex", marginRight:15, marginLeft:5}}
-            max={defaultMax}
-            value={this.state.numCols}
-            onChange={(value) => {this.setState({numCols: value})}}
-          />
-          <Slider style={{display: "flex", width:"350px", marginRight:10}} defaultValue={5} value={this.state.numCols}  min={1} max={defaultMax} onChange={(value) => {this.setState({numCols: value})} }/>
-        </div>
+      { type === "number" && chartType !== "boxPlot" &&
+        <div>
+          <div style={{display: "flex", justifyContent: "left", marginBottom: 10, alignItems: "center"}}>
+            <h4>Interval: </h4>
+            <InputNumber min={1}
+              style={{display: "flex", marginRight:15, marginLeft:5}}
+              max={defaultMax}
+              value={this.state.numCols}
+              onChange={(value) => {this.setState({numCols: value})}}
+            />
+            <Slider style={{display: "flex", width:"350px", marginRight:10}} defaultValue={5} value={this.state.numCols}  min={1} max={defaultMax} onChange={(value) => {this.setState({numCols: value})} }/>
+          </div>
 
-        <div style={{display: "flex", justifyContent: "left", alignItems: "center"}}>
-          <h4 style={{verticalAlign:"middle"}}>Min: </h4>
-          <InputNumber min={defaultMin}
-            max={defaultMax}
-            style={{display: "flex", marginRight:10, marginLeft:5}}
-            value={rangeMin ? rangeMin : defaultMin}
-            onChange={(value) => {if(value<rangeMax){this.setState({rangeMin: value})}}}
-          />
+          <div style={{display: "flex", justifyContent: "left", alignItems: "center"}}>
+            <h4 style={{verticalAlign:"middle"}}>Min: </h4>
+            <InputNumber min={defaultMin}
+              max={defaultMax}
+              style={{display: "flex", marginRight:10, marginLeft:5}}
+              value={rangeMin ? rangeMin : defaultMin}
+              onChange={(value) => this.onRangeMinChange(value, defaultMin, defaultMax)}
+            />
 
-          <h4>Max: </h4>
-          <InputNumber min={defaultMin}
-            max={defaultMax}
-            style={{display: "flex", marginLeft:5, marginRight:15}}
-            value={rangeMax ? rangeMax : defaultMax}
-            onChange={(value) => { if(value>rangeMin){this.setState({rangeMax: value});}}}
-          />
-          <Slider range 
-            defaultValue={[defaultMin, defaultMax]} 
-            value={rangeMax && rangeMin ? [rangeMin, rangeMax] : [defaultMin, defaultMax]} 
-            style={{display: "flex", width:"350px", marginRight:10}}
-            min={defaultMin} 
-            max={defaultMax}
-            onChange={(value) => {this.setState({rangeMin: value[0], rangeMax: value[1]});}}/>
+            <h4>Max: </h4>
+            <InputNumber
+              min={defaultMin}
+              max={defaultMax}
+              style={{display: "flex", marginLeft:5, marginRight:15}}
+              value={rangeMax ? rangeMax : defaultMax}
+              onChange={(value) => this.onRangeMaxChange(value, defaultMin, defaultMax)}
+            />
+            <Slider range
+              defaultValue={[defaultMin, defaultMax]}
+              value={[rangeMin?rangeMin:defaultMin, rangeMax?rangeMax:defaultMax]} 
+              style={{display: "flex", width:"350px", marginRight:10}}
+              min={defaultMin}
+              max={defaultMax}
+              onChange={(value) => {this.setState({rangeMin: value[0], rangeMax: value[1]});}}/>
+          </div>
         </div>
-      </div>
       }
 
       { chartType==="boxPlot" &&
@@ -425,11 +404,11 @@ class VisualisationModal extends React.Component {
 
 const mapStateToProps = (state) => {
   const { 
-    visualisation_visible, error, build, data, visualise, isRowWise
+    visualisation_visible, error, build, data, visualise, isRowWise, record
   } = state.view;
   
   return { 
-    visualisation_visible, error, build, data, visualise, isRowWise
+    visualisation_visible, error, build, data, visualise, isRowWise, record
   };
 };
 
