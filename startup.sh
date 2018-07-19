99650c116a7f1447145900333520379b65d3e20d
Steven Phaedonos <stevenphaedonos@gmail.com>
2018-07-20 00:58:10 +1000

    Include frontend build files

--- startup.sh
+++ startup.sh
@@ -5,17 +5,14 @@ pip3 install -r backend/requirements.txt
 
 uwsgi --ini backend/uwsgi.ini
 
-npm install --prefix frontend
-npm run build --prefix frontend
-
 if [ "$ONTASK_DEMO" = "true" ]
 then
     sudo rm -rf /var/www/html/ontask_demo
-    sudo mv frontend/build /var/www/html/ontask_demo
+    sudo cp -R frontend/build /var/www/html/ontask_demo
 else
     circusd backend/circus.prod.ini --daemon
     sudo rm -rf /var/www/html/ontask
-    sudo mv frontend/build /var/www/html/ontask
+    sudo cp -R frontend/build /var/www/html/ontask
 
     echo "-------------------------------------------------------------------"
     echo "- Servers are now running. To terminate, enter '. ./terminate.sh' -"
