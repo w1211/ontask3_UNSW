f8df3d701393a0a5874aa9aa626efc60f9b9e77b
Steven Phaedonos <steven@phaedonos.com>
2018-10-29 14:55:10 +1100

    Clean up container serialization, remove retrieve_containers endpoint

--- backend/container/serializers.py
+++ backend/container/serializers.py
@@ -10,19 +10,21 @@ from datalab.models import Datalab
 from workflow.models import Workflow
 
 
-class EmbeddedDatasourceSerializer(EmbeddedDocumentSerializer):
+class DatasourceSerializer(EmbeddedDocumentSerializer):
     class Meta:
         model = Datasource
         fields = ["id", "name", "connection", "schedule", "lastUpdated"]
 
 
-class EmbeddedDatalabSerializer(EmbeddedDocumentSerializer):
+class DatalabSerializer(EmbeddedDocumentSerializer):
+    modules = serializers.IntegerField()
+
     class Meta:
         model = Datalab
-        fields = ["id", "name", "steps"]
+        fields = ["id", "name", "modules"]
 
 
-class EmbeddedWorkflowSerializer(EmbeddedDocumentSerializer):
+class ActionSerializer(EmbeddedDocumentSerializer):
     datalab = serializers.CharField()
 
     class Meta:
@@ -31,11 +33,32 @@ class EmbeddedWorkflowSerializer(EmbeddedDocumentSerializer):
 
 
 class ContainerSerializer(DocumentSerializer):
-    datasources = EmbeddedDatasourceSerializer(
-        many=True, allow_null=True, read_only=True
-    )
-    datalabs = EmbeddedDatalabSerializer(many=True, allow_null=True, read_only=True)
-    workflows = EmbeddedWorkflowSerializer(many=True, allow_null=True, read_only=True)
+    datasources = serializers.SerializerMethodField()
+    datalabs = serializers.SerializerMethodField()
+    actions = serializers.SerializerMethodField()
+
+    def get_datasources(self, container):
+        datasources = Datasource.objects(container=container.id)
+        serializer = DatasourceSerializer(datasources, many=True)
+        return serializer.data
+
+    def get_datalabs(self, container):
+        datalabs = Datalab.objects(container=container.id)
+
+        for datalab in datalabs:
+            datalab.modules = len(datalab.steps)
+
+        serializer = DatalabSerializer(datalabs, many=True)
+        return serializer.data
+
+    def get_actions(self, container):
+        actions = Workflow.objects(container=container.id)
+
+        for action in actions:
+            action.datalab = action.datalab.name
+
+        serializer = ActionSerializer(actions, many=True)
+        return serializer.data
 
     class Meta:
         model = Container
