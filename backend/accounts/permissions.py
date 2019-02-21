9b0660749c419eacfc4a37ccb170f5508206f0d1
Steven Phaedonos <steven@phaedonos.com>
2019-02-22 02:12:31 +1100

    CanCreateObjects permission class

---
+++ backend/accounts/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import BasePermission
+
+
+class CanCreateObjects(BasePermission):
+    """
+    Only allow admins or instructors to create containers, datasources, etc.
+    """
+
+    def has_permission(self, request, view):
+        permissable_groups = ["owner", "instructor"]
+        return any(
+            [group.name in permissable_groups for group in request.user.groups.all()]
+        )
